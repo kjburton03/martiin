ad8145b4a6c05a78710fdeeb771e8a5dc1370874
kimberly burton <kj.burton03@gmail.com>
2026-04-13 17:18:41 -0400

    added tips and locations

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,16 @@ import { generatefishHTML } from './fishList.js'
 import { generateLocationHTML } from './locationList.js'
 import { renderFishToDOM } from './fishrender.js'
 import { renderLocationToDom } from './locationRender.js';
+import { renderTipToDOM } from './tipRender.js';
+import { generateTipHTML } from './tipList.js';
 
 // Generate the fish list
 const fishHTML = generatefishHTML()
 renderFishToDOM(fishHTML);
 
 // // Generate the care tips
-// const tipHTML = tipList()
+const tipHTML = generateTipHTML()
+renderTipToDOM(tipHTML);
 
 // // Generate the location list
 const locationHTML = generateLocationHTML()
